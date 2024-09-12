0c5723bf7581c79a6af1e3b0bab852797fb5ecff
lcorream-110 <lcorream@lcorream.xyz>
2024-09-12 12:30:25 +0200

    Update remove_extra_plus.lua

--- remove_extra_plus.lua
+++ remove_extra_plus.lua
@@ -1,7 +1,13 @@
 M = {}
 function M.outbound_INVITE(msg)
-    local from = msg:getHeader("P-Asserted-Identity")
+    local pai = msg:getHeader("P-Asserted-Identity")
+    local from = msg:getHeader("From")
+    local contact = msg:getHeader("Contact")
+    local newpai = string.gsub(pai, "++" ,"+")
     local newfrom = string.gsub(from, "++" ,"+")
-    msg:modifyHeader("P-Asserted-Identity", newfrom)
+    local newcontact = string.gsub(contact, "++" ,"+")
+    msg:modifyHeader("P-Asserted-Identity", newpai)
+    msg:modifyHeader("From", newfrom)
+    msg:modifyHeader("Contact", newcontact)
 end
 return M
