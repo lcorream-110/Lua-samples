bc2dc21241e2baf8864d85af999608ab16ccbf89
lcorream-110 <lcorream@lcorream.xyz>
2024-09-13 18:55:58 +0200

    Update force_sendrecv_sdp.lua

--- force_sendrecv_sdp.lua
+++ force_sendrecv_sdp.lua
@@ -6,7 +6,7 @@ function M.outbound_INVITE(msg)
         local connection = sdp:getLine("a=","inactive")
         if connection
         then
-            --Replace G.722 with G722. The dot is special and must be escaped using % when using gsub.
+            --Replace inactive with sendrecv via gsub.
             connection = connection:gsub("inactive", "sendrecv")
             sdp = sdp:modifyLine("a=", "inactive", connection)
             msg:setSdp(sdp)
