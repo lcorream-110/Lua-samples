1cb9f2b506cc89881afeeb38afddb1fb9d3c176b
lcorream-110 <lcorream@lcorream.xyz>
2024-09-19 15:57:44 +0200

    Update modify-h264-profile-id.lua

--- modify-h264-profile-id.lua
+++ modify-h264-profile-id.lua
@@ -1,10 +1,9 @@
 M = {}
- function M.outbound_ANY(msg)
- 	local sdp = msg:getSdp()
- 	if sdp
- 	then
- 		sdp = sdp:gsub("42E01F", "42E02F")     
- 		msg:setSdp(sdp)
- 	end
- end
- return M
+function M.outbound_ANY(msg)
+ local sdp = msg:getSdp()
+	if sdp 	then
+		sdp = sdp:gsub("42E01F", "42E02F")     
+		msg:setSdp(sdp)
+	end
+end
+return M
