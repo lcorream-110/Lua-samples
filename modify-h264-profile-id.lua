f4a26384a30e1a44590f89ac6259883f163d5b3c
lcorream-110 <lcorream@lcorream.xyz>
2024-09-27 08:42:30 +0200

    Update modify-h264-profile-id.lua

--- modify-h264-profile-id.lua
+++ modify-h264-profile-id.lua
@@ -2,6 +2,7 @@ M = {}
 function M.outbound_ANY(msg)
  local sdp = msg:getSdp()
 	if sdp 	then
+		-- Here you can change these values to the currently received value and desired result.
 		sdp = sdp:gsub("42E01F", "42E02F")     
 		msg:setSdp(sdp)
 	end
